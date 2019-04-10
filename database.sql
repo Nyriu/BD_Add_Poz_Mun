7643e8d2158af366046b398426a7b0cf1c36ba51
Nyriu <muninitristano@gmail.com>
2019-04-10 22:35:32 +0200

    trigger quasi ok

--- database.sql
+++ database.sql
@@ -1,21 +1,21 @@
-set search_path to ospedale;
+drop schema ospedale cascade;
 
-drop table terapia_prescritta;
-drop table terapia;
-drop table diagnosi;
-drop table contiene;
-drop table farmaco;
-drop table principio_attivo;
-drop table ricovero;
-drop table paziente;
+-- set search_path to ospedale;
+-- drop table terapia_prescritta; drop table terapia;
+-- drop table diagnosi;
+-- drop table contiene;
+-- drop table farmaco;
+-- drop table principio_attivo;
+-- drop table ricovero;
+-- drop table paziente;
+-- 
+-- drop domain dom_cf;
+-- drop domain dom_dia;
+-- drop domain dom_ric;
+-- drop domain dom_ter;
+-- drop domain ICD10;
+-- drop schema ospedale;
 
-drop domain dom_cf;
-drop domain dom_dia;
-drop domain dom_ric;
-drop domain dom_ter;
-drop domain ICD10;
-
-drop schema ospedale;
 
 ----------------------------------------------------------------
 -- COPIARE DA QUI IN POI PER CREARE LE TABELLE LA PRIMA VOLTA --
@@ -127,23 +127,107 @@ create table terapia_prescritta (
 );
 
 
-create or replace function ricalcolo_gg(cf_paz)
+create or replace function ricalcolo_gg(cf_paz dom_cf)
 returns int
 language plpgsql as $$
     declare
         gg int;
     begin
-
-        select sum(ricovero.data_f - ricovero.data_i) into gg
-        from paziente join ricovero on paziente.cf = ricovero.paziente
-        where paziente.cf = cf_paz ;
-
+        -- select sum(age(ricovero.data_f, ricovero.data_i)) into gg
+        select sum(ricovero.data_f-ricovero.data_i) into gg
+        from paziente
+          join ricovero on paziente.cf = ricovero.paziente
+        where paziente.cf = cf_paz;
         return gg;
     end
+$$;
+
+
+
+-----------------------------------------------------------------------
+-- CONSTRAIN --
+-----------------------------------------------------------------------
+
+-- CONSTRAINTS sulle date
+  -- RICOVERO e TERAPIA_PRESCRITTA data_i < data_f
+  -- DIAGNOSI deve avere data compresa in data_i e data_f del RICOVERO
+  -- data_i di TERAPIA_PRESCRITTA deve essere uguale o successiva alla data dela diagnosi
+create or replace function check_date_valide(data_i date, data_f date)
+returns bool as
 $$
+  begin
+    return data_i < data_f;
+  end;
+$$ language plpgsql;
+
+alter table ricovero add
+  constraint check_date_valide_ric
+    check(check_date_valide(data_i, data_f));
+
+-- TODO testing
+alter table terapia_prescritta add
+  constraint check_date_valide_dia
+    check(check_date_valide(data_i, data_f));
 
 
+-- data diagnosi compresa in data_i data_f ricovero
+create or replace function check_data_valida(data_dia date, ric dom_ric)
+returns bool as
+$$
+  begin
+    perform *
+    from ricovero
+    where ric = cod_ric and
+          data_i <= data_dia  and
+          (data_f >= data_dia or
+            data_f is null);
+    return found;
+  end;
+$$ language plpgsql;
 
+alter table diagnosi add
+  constraint check_data_valida_dia
+    check(check_data_valida(data_dia, ricovero));
+
+
+-- data_i terapia_prescritta uguale o successiva data_i ricovero
+create or replace function check_data_valida_ter_pre(data date, dia dom_dia)
+returns bool as
+$$
+  begin
+    perform *
+    from diagnosi
+    where dia = cod_dia;
+    return found;
+  end;
+$$ language plpgsql;
+
+-- TODO testing
+alter table terapia_prescritta add
+  constraint check_data_valida_t_p
+    check(check_data_valida_ter_pre(data_i, diagnosi));
+
+
+-- TERAPIA_PRESCRITTA DIAGNOSI->DIAGNOSI no uguale o precedente
+    -- aggiunta nuova T_P
+    -- modifica dei campi
+create or replace function check_dia_valide(dia dom_dia, coll_dia dom_dia)
+returns bool as
+$$
+  begin
+    perform *
+    from diagnosi d1, diagnsi d2
+    where d1.cod_dia = dia and
+          d2.cod_dia = coll_dia and
+          d1.data_dia <= d2.data_dia;
+    return found;
+  end;
+$$ language plpgsql;
+
+-- TODO testing
+alter table terapia_prescritta add
+  constraint check_dia_valide
+    check(check_dia_valide(diagnosi, coll_dia));
 
 
 -----------------------------------------------------------------------
@@ -154,14 +238,61 @@ $$
     -- quando aggiungi PAZIENTE
     -- quando rimuovi RICOVERO
     -- quando modifichi campo Paziente in RICOVERO
+  -- TODO nella relazione scrivere che questo non va implementato per evitare dipendenze cicliche
+  -- Soluzione: creare una funzione che di tanto in tanto pulisca i pazienti senza ricoveri
+  -- (per il nostro dominio è una cosa strana che vengano cancellati ricoveri)
+
 
 -- Totale_Giorni_Ricoveri
-    -- quando aggiungi PAZIENTE (inizializzazione?)
     -- quando aggiungi RICOVERO
     -- quando rimuovi RICOVERO
     -- quando modifichi campo Data Inizio in RICOVERO
     -- quando modifichi campo Data Fine in RICOVERO
     -- quando modifichi campo Paziente in RICOVERO
+create or replace function aggiorna_gg_insert()
+returns trigger as
+$$
+  begin
+    -- inserito nuovo ric
+    update paziente
+    set tot_gg_ric = ricalcolo_gg(new.paziente)
+    where paziente.cf = new.paziente;
+    return new;
+  end;
+$$ language plpgsql;
+
+create or replace function aggiorna_gg_update()
+returns trigger as
+$$
+  begin
+    update paziente
+    set tot_gg_ric = ricalcolo_gg(new.paziente)
+    where paziente.cf = new.paziente;
+
+    if new.paziente <> old.paziente
+      then
+      update paziente
+      set tot_gg_ric = ricalcolo_gg(old.paziente)
+      where paziente.cf = old.paziente;
+    end if;
+
+    return new;
+  end;
+$$ language plpgsql;
+
+create trigger aggiornamento_gg_insert after insert
+  on ricovero for each row
+  execute procedure aggiorna_gg_insert();
+
+create trigger aggiornamento_gg_update after update
+  on ricovero for each row
+    when ( new.data_i <> old.data_i or
+           new.data_f <> old.data_f or
+           old.data_i is null       or
+           old.data_f is null       or
+           new.paziente <> old.paziente )
+      execute procedure aggiorna_gg_update();
+
 
 -- DIAGNOSI una sola TERAPIA_PRESCRITTA TODO BASTA UNIQUE!!
     -- Diagnosi in T_P deve essere chiave, deve essere introdotto un vincolo che asscuri che una Diagnosi compaia una sola volta dentro tutta la tabella delel T_P
@@ -169,23 +300,59 @@ $$
 -- DIAGNOSI è effetto collaterale di una sola TERAPIA_PRESCRITTA TODO BASTA UNIQUE!!
     -- coll_dia in T_P deve essere chiave, deve essere introdotto un vincolo che asscuri che una coll_dia compaia una sola volta dentro tutta la tabella delel T_P
 
--- TERAPIA_PRESCRITTA DIAGNOSI->DIAGNOSI no uguale o precedente
-    -- modifica campo Effetto_Collaterale
-    -- problema modifica Diagnsi in T_P non si pone perchè in quel caso disfi e rifai tupla
 
 -- TERAPIA almeno una TERAPIA_PRESCRITTA
     -- quando aggiungi TERAPIA
     -- quando rimuovi TERAPIA_PRESCRITTA
     -- problema modifica Terapia in T_P non si pone perchè in quel caso disfi e rifai tupla
+  -- se si facesse si introdurrebbe una dipendenza ciclica
+  -- non potrei inserire una TERAPIA se prima non ho già una TERAPIA_PRESCRITTA
+  -- cosa impossibile perche' T_P ha terapia come pezzo di chiave primaria
+  -- TODO soluzione simile a quella per "PAZIENTE almeno un RICOVERO"
 
--- FARMACO almeno un PRINCIPIO_ATTIVO
-  -- il check da fare su CONTIENE
-    -- quando aggiungi FARMACO
-    -- quando rimuovi PRINCIPIO_ATTIVO
-    -- quando rimuovi tupla da CONTIENE
+
 
 -- PRINCIPIO_ATTIVO almeno un FARMACO
   -- il check da fare su CONTIENE
     -- quando aggiungi PRINCIPIO_ATTIVO
     -- quando rimuovi FARMACO
     -- quando rimuovi tupla da CONTIENE
+    -- quando aggiorni tupla da CONTIENE
+-- FARMACO almeno un PRINCIPIO_ATTIVO
+  -- il check da fare su CONTIENE
+    -- NO -- quando aggiungi FARMACO
+    -- quando rimuovi PRINCIPIO_ATTIVO TODO
+    -- quando rimuovi tupla da CONTIENE
+    -- quando aggiorni tupla da CONTIENE
+-- Per evitare dipendenze cicliche permetto ad un P_A di rimanere senza FARMACI
+-- TODO soluzione simile a quella per "PAZIENTE almeno un RICOVERO"
+
+create or replace function almeno_un_pa_cont()
+returns trigger as
+$$
+  begin
+    perform *
+    from contiene
+    where old.farmaco == farmaco and
+          old.pr_attivo <> pr_attivo;
+    if not found
+    then
+      raise exception 'Rimarrebbe senza principi attivi'; -- TODO dire meglio
+      return null;
+    else
+      return new;
+    end if;
+  end;
+$$ language plpgsql;
+
+-- TODO testing
+create trigger controlla_almeno_pa_del before delete
+  on contiene for each row
+    execute procedure almeno_un_pa_cont();
+
+create trigger controlla_almeno_pa_up before update
+  on contiene for each row
+    when ( new.farmaco <> old.farmaco or
+           new.pr_attivo <> old.pr_attivo)
+    execute procedure almeno_un_pa_cont();
+
--- tests.sql
+++ tests.sql
@@ -23,3 +23,126 @@ insert into diagnosi(cod_dia, cod_pat)
 	('DIA250','CAA4.3435.366');
 
 select cod_dia, cod_pat from diagnosi;
+
+
+---------------------------------------------------
+-- TESTING su ricalcolo_gg
+---------------------------------------------------
+
+insert into paziente(cf,cognome, nome, data_nasc, luogo_nasc, prov_res) values
+  ('ABCDEF01A01A012A', 'Belli', 'Armando', '2000-01-01', 'Abbabba', 'AB')
+  ;
+
+insert into ricovero(cod_ric, data_i, motivo, div_osp, paziente) values
+  ('RIC001', '2000-01-01', 'a', 'd', 'ABCDEF01A01A012A'),
+  ('RIC002', '2000-02-01', 'a', 'd', 'ABCDEF01A01A012A'),
+  ('RIC003', '2000-03-01', 'a', 'd', 'ABCDEF01A01A012A')
+  ;
+
+
+update ricovero SET data_f = '2000-01-11' where cod_ric = 'RIC001';
+update ricovero SET data_f = '2000-02-11' where cod_ric = 'RIC002';
+
+
+-- update per tutti i pazienti
+update paziente SET tot_gg_ric = ricalcolo_gg(cf);
+
+
+---------------------------------------------------
+-- TESTING TRIGGERS su tot_gg_ric
+---------------------------------------------------
+
+
+set search_path to ospedale;
+insert into paziente(cf,cognome, nome, data_nasc, luogo_nasc, prov_res) values
+  ('ABCDEF01A01A012A', 'Belli', 'Armando', '2000-01-01', 'Abbabba', 'AB'),
+  ('ABCDEF01A01A012B', 'Celli', 'Armando', '2000-01-01', 'Abbabba', 'AB')
+  ;
+
+select cf, tot_gg_ric from paziente;
+
+insert into ricovero(cod_ric, data_i, data_f, motivo, div_osp, paziente) values
+  ('RIC001', '2000-01-05', '2000-01-10', 'a', 'd', 'ABCDEF01A01A012A'),
+  ('RIC002', '2000-01-01', '2000-01-21', 'a', 'd', 'ABCDEF01A01A012B')
+  ;
+
+select cf, tot_gg_ric from paziente; -- si aggiorna
+
+insert into ricovero(cod_ric, data_i, motivo, div_osp, paziente) values
+  ('RIC003', '2000-02-01', 'a', 'd', 'ABCDEF01A01A012A'),
+  ('RIC004', '2000-02-01', 'a', 'd', 'ABCDEF01A01A012B')
+  ;
+
+select cf, tot_gg_ric from paziente; -- rimane uguale 
+
+update ricovero set data_i = '2000-01-01' where cod_ric = 'RIC001';
+update ricovero set data_f = '2000-02-26' where cod_ric = 'RIC004';
+
+select cf, tot_gg_ric from paziente; -- si aggiorna
+
+
+---------------------------------------------------
+-- TESTING CONSTRAIN su data_i e data_f
+---------------------------------------------------
+
+set search_path to ospedale;
+insert into paziente(cf,cognome, nome, data_nasc, luogo_nasc, prov_res) values
+  ('ABCDEF01A01A012A', 'Belli', 'Armando', '2000-01-01', 'Abbabba', 'AB'),
+  ('ABCDEF01A01A012B', 'Celli', 'Armando', '2000-01-01', 'Abbabba', 'AB')
+  ;
+
+insert into ricovero(cod_ric, data_i, data_f, motivo, div_osp, paziente) values
+  ('RIC001', '2000-01-05', '2000-01-10', 'a', 'd', 'ABCDEF01A01A012A'),
+  ('RIC002', '2000-01-01', '2000-01-21', 'a', 'd', 'ABCDEF01A01A012B')
+  ;
+
+insert into ricovero(cod_ric, data_i, motivo, div_osp, paziente) values
+  ('RIC003', '2000-02-01', 'a', 'd', 'ABCDEF01A01A012A'),
+  ('RIC004', '2000-02-01', 'a', 'd', 'ABCDEF01A01A012B')
+  ;
+
+update ricovero set data_i = '2000-01-01' where cod_ric = 'RIC001';
+update ricovero set data_f = '2000-02-26' where cod_ric = 'RIC004';
+
+update ricovero set data_f = '2000-01-26' where cod_ric = 'RIC003'; -- deve fallire
+update ricovero set data_f = '1999-02-26' where cod_ric = 'RIC003'; -- deve fallire
+update ricovero set data_i = '2000-03-01' where cod_ric = 'RIC001'; -- deve fallire
+
+-- testing su terapia prescritta TODO completare
+insert into farmaco (azienda_prod, dose_gg_racc, nome_comm) values
+  ('ciccio',1,'AAA')
+  ;
+
+-- testing su data diagnosi
+insert into diagnosi (cod_dia, data_dia, cod_pat, grav_pat, medico, paziente, ricovero) values
+  ('DIA01', '2000-03-01', 'T10.103040', 'true', 'Gigio', 'ABCDEF01A01A012B', 'RIC003'), -- stesso giorno di inizio ricvero (ric senza fine)
+  ('DIA02', '2000-01-01', 'T10.103040', 'true', 'Gigio', 'ABCDEF01A01A012A', 'RIC001')  -- stesso giorno di inizio ricvero (ric con fine)
+  ;
+update diagnosi set data_dia = '2000-03-02' where cod_dia = 'DIA01'; -- okay (ric senza fine)
+update diagnosi set data_dia = '2000-01-08' where cod_dia = 'DIA02'; -- okay
+
+  -- devono fallire
+insert into diagnosi (cod_dia, data_dia, cod_pat, grav_pat, medico, paziente, ricovero) values
+  ('DIA03', '2000-02-01', 'T10.103040', 'true', 'Gigio', 'ABCDEF01A01A012B', 'RIC003'); -- prima di data_i(ric senza fine)
+insert into diagnosi (cod_dia, data_dia, cod_pat, grav_pat, medico, paziente, ricovero) values
+  ('DIA04', '2000-05-01', 'T10.103040', 'true', 'Gigio', 'ABCDEF01A01A012A', 'RIC001'); -- dopo data_f
+
+update diagnosi set data_dia = '1999-03-02' where cod_dia = 'DIA01'; -- fallire (ric senza fine)
+update diagnosi set data_dia = '2000-01-18' where cod_dia = 'DIA02'; -- fallire
+
+-- testing su terapia prescritta TODO completare
+
+
+
+---------------------------------------------------
+-- TESTING CONSTRAIN su T_P dia ed coll_dia
+---------------------------------------------------
+
+-- TODO
+
+
+---------------------------------------------------
+-- TESTING TRIGGERS su farmaco alemno un pr_att
+--------------------------------------------------
+
+-- TODO
