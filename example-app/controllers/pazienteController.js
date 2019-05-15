c10f77cadeec425b54f54541393046cbf81467a3
Samuele <samuelepoz13@gmail.com>
2019-05-15 20:56:27 +0200

    fix

--- example-app/controllers/pazienteController.js
+++ example-app/controllers/pazienteController.js
@@ -55,7 +55,7 @@ const controller = {
     ];
     pool.query(
       `INSERT INTO ospedale.paziente
-      VALUES($1, $2, $3, $4, $6, $7, $8, $9)`, // query end
+      VALUES( $1, $2, $3, $4, $5, $6, $7, $8, $9)`, // query end
       values,
       (err, queryRes) => {
         if (err) {
